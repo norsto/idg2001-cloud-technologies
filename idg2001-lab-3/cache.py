87ad8b37864552cc034a576a08b2e2983dbe6c6b
norsto <norajr@storroe.no>
2025-01-23 19:28:47 +0100

    Commented the cache task and actually understood what's going on

--- idg2001-lab-3/cache.py
+++ idg2001-lab-3/cache.py
@@ -1,24 +1,32 @@
-"""
-print(f)
-print("hello?")
-print("it is working")
-"""
-
+# (Initially) an empty dictionary called cache
 cache = {}
 
+# Returns the square of x
 def f(x):
     return x ** 2
 
+# Checks if the result for x is already stored in the cache
 def cached_f(x):
-#    global cache
-#    if x in cache:
-    return cache[x]  # Return cached result
-#    else:
-#        result = f(x)  # Calculate result
-#        cache[x] = result  # Store result in cache
-#        return result
+    if x in cache:
+    # If x has alreeady been calculated before (it'll be in the cache), --
+    # the function will return the stored value instead of recalculating it
+        return cache[x]  
+    # Else, if not in cache--
+    else:
+        # Calculates result/the x
+        result = f(x)  
+        # Stores result in cache
+        cache[x] = result  
+        # Returns the result
+        return result
 
 
-print(cached_f(3))
-print(cached_f(3))
-print(cached_f(3))
+print(cache)        # Initially empty: {}
+
+print(cached_f(3))  # First call, calculates 3**2 = 9, stores the answer in cache
+print(cache)        # Now: {3: 9}
+
+print(cached_f(3))  # Second call, retrieves from cache (no recalculation)
+
+print(cached_f(4))  # First time for 4, calculates 4**2 = 16
+print(cache)        # Now: {3: 9, 4: 16}
